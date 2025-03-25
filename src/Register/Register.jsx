7f6620000b8389e01ac522051a258afdc7609c16
jnakht <jisan.cse.cu@gmail.com>
2025-03-25 23:13:00 +0600

    register styling added

--- src/Register/Register.jsx
+++ src/Register/Register.jsx
@@ -1,9 +1,18 @@
 
 
 const Register = () => {
+    const handleRegister = (e) => {
+        e.preventDefault();
+    }
     return (
         <div>
-            
+            <h3 className="text-3xl mt-[56px] mb-4">Please Register</h3>
+            <form className="flex flex-col gap-4 w-[450px]">
+                <input className="bg-slate-500 py-2 px-4" type="text" placeholder="Your Name" name="name" id="" />
+                <input className="bg-slate-500 py-2 px-4" type="email" placeholder="Email Address" name="email" id="" />
+                <input className="bg-slate-500 py-2 px-4" type="password" placeholder="Password" name="password" id="" />
+                <input className="btn btn-primary" type="submit" value="Submit" />
+            </form>
         </div>
     );
 };
