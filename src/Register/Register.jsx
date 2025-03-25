935b9b5ccd137f064b329530103f249dce45e657
jnakht <jisan.cse.cu@gmail.com>
2025-03-25 23:43:57 +0600

    show password toggle set

--- src/Register/Register.jsx
+++ src/Register/Register.jsx
@@ -1,7 +1,10 @@
 import { createUserWithEmailAndPassword} from "firebase/auth";
 import auth from "../components/firebase/firebase.config";
-
+import { IoEye } from "react-icons/io5";
+import { IoEyeOff } from "react-icons/io5";
+import { useState } from "react";
 const Register = () => {
+    const [showPassword, setShowPassword] = useState(false);
     const handleRegister = (e) => {
         e.preventDefault();
         const userEmail = e.target.email.value;
@@ -12,7 +15,7 @@ const Register = () => {
             console.log(result.user);
         })
         .catch(error => {
-            console.error(error)
+            console.error(error);
         })
     }
     return (
@@ -21,7 +24,17 @@ const Register = () => {
             <form onSubmit={handleRegister} className="flex flex-col gap-4 w-[450px]">
                 <input className="bg-slate-500 py-2 px-4" type="text" placeholder="Your Name" name="name" id="" />
                 <input className="bg-slate-500 py-2 px-4" type="email" placeholder="Email Address" name="email" id="" />
-                <input className="bg-slate-500 py-2 px-4" type="password" placeholder="Password" name="password" id="" />
+                <div className="relative">
+                    <input className="bg-slate-500 py-2 px-4 w-full" type={showPassword ? 'password' : 'text'} placeholder="Password" name="password" id="" />
+                    <span onClick={() => setShowPassword(!showPassword)} className="absolute top-2 right-4 text-2xl">
+                        {
+                            showPassword ? 
+                            <IoEyeOff />
+                            :
+                            <IoEye />
+                        }
+                    </span>
+                </div>
                 <input className="btn btn-primary" type="submit" value="Submit" />
             </form>
         </div>
