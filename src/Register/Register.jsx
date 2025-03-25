81869531b7218249baf209b1eaf4b7080c7f91b4
jnakht <jisan.cse.cu@gmail.com>
2025-03-25 23:16:18 +0600

    name, email, password got

--- src/Register/Register.jsx
+++ src/Register/Register.jsx
@@ -1,13 +1,15 @@
 
-
 const Register = () => {
     const handleRegister = (e) => {
         e.preventDefault();
+        const userEmail = e.target.email.value;
+        const userPassword = e.target.password.value;
+        console.log(userEmail, userPassword)
     }
     return (
         <div>
             <h3 className="text-3xl mt-[56px] mb-4">Please Register</h3>
-            <form className="flex flex-col gap-4 w-[450px]">
+            <form onSubmit={handleRegister} className="flex flex-col gap-4 w-[450px]">
                 <input className="bg-slate-500 py-2 px-4" type="text" placeholder="Your Name" name="name" id="" />
                 <input className="bg-slate-500 py-2 px-4" type="email" placeholder="Email Address" name="email" id="" />
                 <input className="bg-slate-500 py-2 px-4" type="password" placeholder="Password" name="password" id="" />
