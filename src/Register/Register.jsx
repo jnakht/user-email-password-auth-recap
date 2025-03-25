0dfaaebf1d5a184e70344f019c5d0fedfd2a1033
jnakht <jisan.cse.cu@gmail.com>
2025-03-26 00:26:13 +0600

    success and error message showed on registration

--- src/Register/Register.jsx
+++ src/Register/Register.jsx
@@ -6,30 +6,40 @@ import { useState } from "react";
 const Register = () => {
     const [showPassword, setShowPassword] = useState(false);
     const [termsAccepted, setTermsAccepted] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
+    const [successMessage, setSuccessMessage] = useState('');
     const handleTermsAccepted = () => {
         setTermsAccepted(!termsAccepted);
     }
     const handleRegister = (e) => {
         e.preventDefault();
+        setErrorMessage('');
+        setSuccessMessage('');
         const userEmail = e.target.email.value;
         const userPassword = e.target.password.value;
         console.log(userEmail, userPassword);
+        if (userPassword.length < 6) {
+            setErrorMessage('Password must be at least 6 charcaters long');
+            return;
+        }
         createUserWithEmailAndPassword(auth, userEmail, userPassword)
         .then(result => {
             console.log(result.user);
+            setSuccessMessage('Successfully Registered')
         })
         .catch(error => {
             console.error(error);
+            setErrorMessage(error.message)
         })
     }
     return (
         <div>
             <h3 className="text-3xl mt-[56px] mb-4">Please Register</h3>
             <form onSubmit={handleRegister} className="flex flex-col gap-4 w-[450px]">
-                <input className="bg-slate-500 py-2 px-4" type="text" placeholder="Your Name" name="name" id="" />
-                <input className="bg-slate-500 py-2 px-4" type="email" placeholder="Email Address" name="email" id="" />
+                <input className="bg-slate-500 py-2 px-4" type="text" placeholder="Your Name" name="name" id="" required/>
+                <input className="bg-slate-500 py-2 px-4" type="email" placeholder="Email Address" name="email" id="" required/>
                 <div className="relative">
-                    <input className="bg-slate-500 py-2 px-4 w-full" type={showPassword ? 'text' : 'password'} placeholder="Password" name="password" id="" />
+                    <input className="bg-slate-500 py-2 px-4 w-full" type={showPassword ? 'text' : 'password'} placeholder="Password" name="password" id="" required/>
                     <span onClick={() => setShowPassword(!showPassword)} className="absolute top-2 right-4 text-2xl">
                         {
                             showPassword ? <IoEye />   :
@@ -42,6 +52,16 @@ const Register = () => {
                       <p>Accept the terms and conditions</p>
                 </div>
                 <input disabled={!termsAccepted} className="btn btn-primary" type="submit" value="Submit" />
+                {
+                    errorMessage && <div>
+                        <p className="text-3xl text-red-600">{errorMessage}</p>
+                    </div>
+                }
+                {
+                    successMessage && <div>
+                        <p className="text-3xl text-green-600">{successMessage}</p>
+                    </div>
+                }
             </form>
         </div>
     );
